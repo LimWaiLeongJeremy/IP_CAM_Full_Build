a4e7459d224d92810c335050b12860a227be6b22
LimWaiLeongJeremy <jereremy19995@hotmail.sg>
2024-02-13 16:04:29 +0800

    router outlet bug fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './component/home/home.component';
 import { RouterModule, Routes } from '@angular/router';
 import { PlayerComponent } from './component/player/player.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
 import { NavbarComponent } from './component/navbar/navbar.component';
-
-
 
 
 const routes: Routes = [
   {
-    path: '/',
-    title: 'home',
+    path: "",
+    // title: 'home',
     component: HomeComponent
   },
   {
-    path: 'home',
-    title: 'home',
+    path: "home",
+    // title: 'home',
     component: HomeComponent
   },
   {
